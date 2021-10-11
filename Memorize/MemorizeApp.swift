34068af2956b48ddc7cf88c0067bf0837abe664b
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-10-11 16:43:30 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Serge Villasica on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
