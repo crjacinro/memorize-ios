c93210094e748150fa273a1523dcfd9a93aaf044
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-10-13 22:33:01 +0800

    Add emoji array

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["🛻", "🚛","🚖","✈️","🚢","🚝","🚑","🚚","🚔","🛩",]
+    let emojiCount = 5
+    
     var body: some View {
-        Text("Hello, world!")
+        HStack{
+            ForEach(emojis[0..<emojiCount], id: \.self){ emoji in
+                CardView(content: emoji)
+            }
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+    }
+}
+
+struct CardView: View {
+    var content = ""
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack {
+            let cardShape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                cardShape.fill().foregroundColor(.white)
+                cardShape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            }else{
+                RoundedRectangle(cornerRadius: 20)
+                    .fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
